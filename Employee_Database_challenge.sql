1fcc230b964903175c3c58f542c4aeed94e0241a
Brit0812 <britkura@outlook.com>
2022-06-05 23:45:29 -0400

    Final V4

--- Employee_Database_challenge.sql
+++ Employee_Database_challenge.sql
@@ -67,3 +67,55 @@ WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
 AND (de.to_date='9999-01-01')
 ORDER BY e.emp_no;
 
+
+
+
+--Deliverable 3
+--Need 2 tables 
+---what department and roles need to be fill? 
+--whats  needed?
+	-- name- first and last 
+	-- title 
+	-- emp_no 
+	-- dept_name
+--name of table?
+	--dept_roll_fill 
+--pull from the retirement_titles 
+SELECT DISTINCT ON (rt.emp_no)
+rt.emp_no,
+rt.first_name,
+rt.last_name,
+rt.title,
+de.dept_no,
+d.dept_name
+INTO unique_title_depts
+FROM retirement_titles as rt	
+INNER JOIN dept_emp as de
+ON (rt.emp_no = de.emp_no)
+INNER JOIN departments as d 
+ON (d.dept_no = de.dept_no)
+ORDER BY rt.emp_no, rt.to_date DESC;
+
+
+
+SELECT ut.dept_name, ut.title, COUNT(ut.title) 
+INTO dept_role_fill 
+FROM (SELECT title, dept_name from unique_title_depts) as ut
+GROUP BY ut.dept_name, ut.title
+ORDER BY ut.dept_name DESC;
+-- to fill would be  different number? 90k vs the retirees which is 70k?
+
+
+
+--whos qualified to train?
+    --manager 
+    -- seniors (staff, engineer, etc)
+    --leaders
+--are there enough quailed retirees to mentor the next gen emp?
+
+SELECT ut.dept_name, ut.title, COUNT(ut.title) 
+INTO training_staff
+FROM (SELECT title, dept_name from unique_title_depts) as ut
+WHERE ut.title IN ('Manager', 'Senior Staff', 'Technique Leader','Senior Engineer')
+GROUP BY ut.dept_name, ut.title
+ORDER BY ut.dept_name DESC;
